f432ffc651c34eee6ac9b9927af4e0caf2ac59b3
Eric Scott <scottericr@gmail.com>
2023-04-28 20:52:37 +0000

    rerun with some new data

--- R/define_recipe.R
+++ R/define_recipe.R
@@ -5,7 +5,7 @@ define_recipe <- function(data_train) {
     update_role(site, new_role = "id") |> 
     step_log(all_outcomes(), id = "log_resp", skip = TRUE) |> #log transform response to improve normality of residuals.
     step_dummy(all_nominal_predictors()) |> 
-    step_zv(all_predictors()) |> 
+    step_zv(all_predictors()) |> #remove predictors with zero variance
     step_normalize(all_numeric_predictors()) |> 
     prep(training = data_train, retain = TRUE) #not 100% sure what this does
 }
