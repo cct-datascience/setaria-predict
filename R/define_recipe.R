84c6087e175b094e3b3814c461c0052451b144f0
Eric Scott <scottericr@gmail.com>
2023-05-02 14:20:29 +0000

    add more climate vars, interaction between ecocystem and genotype, and a PCA step for the bioclim vars

--- R/define_recipe.R
+++ R/define_recipe.R
@@ -3,15 +3,15 @@ define_recipe <- function(data_train) {
     update_role(everything(), new_role = "predictor") |> 
     update_role(npp_yr10, new_role = "outcome") |> 
     update_role(site, new_role = "id") |> 
-    step_log(all_outcomes(), id = "log_resp", skip = TRUE) |> #log transform response to improve normality of residuals.
-    step_dummy(all_nominal_predictors()) |> 
-    step_zv(all_predictors()) |> #remove predictors with zero variance
     step_normalize(all_numeric_predictors()) |> 
-    prep(training = data_train, retain = TRUE) #not 100% sure what this does
+    step_interact(~genotype:ecosystem) |> 
+    step_dummy(all_nominal_predictors()) |> 
+    step_log(all_outcomes(), id = "log_resp", skip = TRUE) |> #log transform response to improve normality of residuals.
+    step_zv(all_predictors()) |> #remove predictors with zero variance
+    step_pca(starts_with("bio"), threshold = 0.85) |> #maybe not necessary? RMSE is identical with and without, rsq slightly higher with pca. Check literature!
+    prep(training = data_train, retain = TRUE) 
 }
-
+# define_recipe(data_train) |> bake(data_train) |> View()
 #' TODO: 
 #' - Consider step_pca() for bioclim variables since they are likely collinear
 #'   and possibly redundant.  Look to literature to see how others use bioclim.
-#' - Add means of mean_vpd and mean_srad as predictors
-#' - Consider an interaction between genotype and ecosystem as a predictor
