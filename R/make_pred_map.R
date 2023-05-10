bb4b60845ea882c786535441e5ea5cca07d3c293
Eric Scott <scottericr@gmail.com>
2023-05-10 14:41:21 +0000

    beautify maps

--- R/make_pred_map.R
+++ R/make_pred_map.R
@@ -10,6 +10,8 @@
 #' - proportion of biomass
 #' - density
 make_pred_map <- function(grid_pred, seus) {
+  usa <- st_as_sf(maps::map("usa", plot = FALSE, fill = TRUE))
+  
   #create raster of predicted data
   pred_sf <- 
     grid_pred |> 
@@ -19,55 +21,38 @@ make_pred_map <- function(grid_pred, seus) {
   
   #plot raster and state boundaries
   ggplot() +
+    geom_sf(data = usa, fill = "antiquewhite1") +
     geom_stars(data = pred_sf, mapping = aes(fill = NPP), na.action = na.omit) +
     geom_sf(data = seus, fill = NA) +
-    coord_sf() +
-    scale_fill_viridis_c() +
+    geom_sf_label() +
+    facet_grid(genotype ~ ecosystem, labeller = as_labeller(str_to_sentence)) +
+    coord_sf(xlim = st_bbox(seus)[c(1,3)], ylim = st_bbox(seus)[c(2, 4)]) +
+    scale_fill_viridis_c(
+      trans = scales::log_trans(),
+      labels = scales::label_log(digits = 2),
+      breaks = scales::breaks_log(n = 8)
+    ) +
     scale_y_continuous(breaks = c(25, 30, 35, 40)) +
     scale_x_continuous(breaks = c(-75, -80, -85, -90)) +
-    labs(fill = "Summer NPP in year 10 [kg/m^2/s]") +
-    facet_grid(genotype ~ ecosystem, labeller = label_both) +
+    guides(
+      fill = guide_colorbar(
+        title = "Summer NPP in year 10 [kg/m^2/s]",
+        title.position = "top",
+        barwidth = unit(10, "cm"),
+      )) +
     theme_bw() +
     theme(
-      legend.position = "top",
+      legend.position = "bottom",
+      legend.justification = "center",
       axis.title = element_blank(),
-      panel.background = element_rect(fill = "azure")
+      panel.grid.major = element_line(color = "grey50", linetype = "dashed", linewidth = 0.2),
+      panel.background = element_rect(fill = "aliceblue"),
+      axis.text.x = element_text(angle = 45, hjust = 1)
     )
   
 }
 
-# same as above but don't backtransform
-make_pred_map_log <- function(grid_pred, seus) {
-  #create raster of predicted data
-  pred_sf <- 
-    grid_pred |> 
-    dplyr::select(genotype, ecosystem, lat, lon, .pred) |> 
-    mutate(NPP = exp(.pred)) |> 
-    st_as_stars(coords = c("lon", "lat"), dims = c("lon", "lat", "ecosystem", "genotype"))
-  
-  #plot raster and state boundaries
-  ggplot() +
-    geom_stars(data = pred_sf, mapping = aes(fill = NPP), na.action = na.omit) +
-    geom_sf(data = seus, fill = NA) +
-    geom_sf_label() +
-    coord_sf() +
-    scale_fill_viridis_c(
-      trans = scales::log_trans(),
-      labels = scales::label_log(digits = 2),
-      breaks = scales::breaks_log(n = 4)
-    ) +
-    scale_y_continuous(breaks = c(25, 30, 35, 40)) +
-    scale_x_continuous(breaks = c(-75, -80, -85, -90)) +
-    labs(fill = "Summer NPP in year 10 [kg/m^2/s]") +
-    facet_grid(genotype ~ ecosystem, labeller = label_both) +
-    theme_bw() +
-    theme(
-      legend.position = "top",
-      axis.title = element_blank(),
-      panel.background = element_rect(fill = "azure")
-    )
-  
-}
+
 
 #' Map proportional difference from wildtype at all ecosystems for all genotypes
 #'
@@ -95,7 +80,7 @@ make_pred_map_diff <- function(grid_pred, seus) {
       na.action = na.omit
     ) +
     geom_sf(data = seus, fill = NA) +
-    facet_grid(genotype ~ ecosystem, labeller = label_both) +
+    facet_grid(genotype ~ ecosystem, labeller = as_labeller(str_to_sentence)) +
     coord_sf(xlim = st_bbox(seus)[c(1,3)], ylim = st_bbox(seus)[c(2, 4)]) +
     colorspace::scale_fill_continuous_diverging(
       palette = "Blue-Red",
@@ -117,7 +102,8 @@ make_pred_map_diff <- function(grid_pred, seus) {
       legend.justification = "center",
       axis.title = element_blank(),
       panel.grid.major = element_line(color = "grey50", linetype = "dashed", linewidth = 0.2),
-      panel.background = element_rect(fill = "aliceblue")
+      panel.background = element_rect(fill = "aliceblue"),
+      axis.text.x = element_text(angle = 45, hjust = 1)
     )
   
 }
